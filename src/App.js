e475afb01a659a904d19e181bcee29b44f332a0e
Nidhi <kumari.nidhi4488@gmail.com>
2022-07-24 18:28:10 +0530

    initial/first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./Components/HeadersComponents/Header";
+import AboutUs from "./Components/BodyComponents/AboutUs";
+import Portfolio from "./Components/BodyComponents/Portfolio";
+import Contact from "./Components/BodyComponents/Contact";
+import Footer from "./Components/BodyComponents/Footer";
+// npm i @material-ui/core @material-ui/icons
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+       <Header />
+       <AboutUs />
+       <Portfolio />
+       <Contact />
+       <Footer /> 
+
+      </>
+        
     </div>
   );
 }
---
+++ src/Components/Theme.jsx
@@ -0,0 +1,16 @@
+export const Theme = {
+    colors: {
+      base1: "#C70039", // deep green backgrounf [navbar, about , contact]
+      base2: "#fff", //white
+      primary: "#0097a7", //lighter green
+      primary1: "#143438", // deepest green [portfolio, footer]
+    },
+
+    // colors: {
+    //   base1: "#1e464a", // deep green backgrounf [navbar, about , contact]
+    //   base2: "#fff", //white
+    //   primary: "#0097a7", //lighter green
+    //   primary1: "#143438", // deepest green [portfolio, footer]
+    // },
+
+};
